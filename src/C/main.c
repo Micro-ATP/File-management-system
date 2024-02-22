f93a469a6c93c7c85d19dce9cf72b7a640589d60
陆岩淅 <131521928+Micro-ATP@users.noreply.github.com>
2024-02-22 16:17:56 +0800

    修复严重错误，避免了输入指令直接exit的现象

--- src/C/main.c
+++ src/C/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#include <E:\GitHub\File-management-system\C\help.h>
-#include <E:\GitHub\File-management-system\C\neofetch.h>
+// #include <E:\GitHub\File-management-system\src\C\help.h>
+#include <E:\GitHub\File-management-system\src\C\neofetch.h>
 
 void createFile(char *filename);
 void readFile(char *filename);
@@ -11,9 +11,10 @@ void deleteFile(char *filename);
 void listFiles();
 void openFile(char *filename);
 void changeDirectory(char *newDirectory);
-void searchFiles(char *target_name);
+// void searchFiles(char *target_name);
+void help_log();
 
-void global_search(char *target_name, char *directory);
+// void global_search(char *target_name, char *directory);
 
 void createFile(char *filename) {
     FILE *file = fopen(filename, "w");
@@ -148,8 +149,30 @@ void changeDirectory(char *newDirectory) {
 //     FindClose(hFind);
 // }
 
+void help_log() {
+    printf("\n");
+    printf("Welcome to ATP_Shell, the friendly interactive shell !!!\n\n");
+    printf("    Commands example:\n\n");
+    printf("        ls - List files and directories in the current directory\n");
+    printf("        touch <file_name> - Create a new file\n");
+    printf("        rm -f <file_name> - Delete a file\n");
+    printf("        open <file_name> - Open a file\n");
+    printf("        cd <directory> - Change the current directory\n");
+    printf("        whereis <file_name> - Search for a file\n");
+    printf("        everything <file_name> - Search for files using Everything\n");
+    printf("        neofetch - Display system information\n");
+    printf("        fetchsoft - List all installed software\n");
+    printf("        unsoft <software_name> - Uninstall a software\n");
+    printf("        clear - Clear the screen\n");
+    printf("        diskpart - Open diskpart\n");
+    printf("        chkssd - Check SSD health\n");
+    printf("        quit - Exit the shell\n");
+    printf("        exit - Exit the shell\n");
+    printf("        help - Display this help message\n");
+    printf("\n");
+}
+
 int main() {
-    char filename[50];
     int choice;
 
     char command[256]; // Command line input buffer
@@ -157,7 +180,7 @@ int main() {
     char openCommand[256]; // Buffer for storing open command and its arguments
     char rmCommand[256]; // Buffer for storing rm command and its arguments
     char tchCommand[256]; // Buffer for storing touch command and its arguments
-    char wheisCommand[256]; // Buffer for storing whereis command and its arguments
+    // char wheisCommand[256]; // Buffer for storing whereis command and its arguments
 
     char current_directory[MAX_PATH] = "C:\\"; // Set the current directory to "C:\"
 
@@ -173,11 +196,11 @@ int main() {
         // 读取命令
         fgets(command, sizeof(command), stdin);
 
-        // 去除命令末尾的换行符
-        size_t command_len = strlen(command);
-        if (command[command_len - 1] == '\n') {
-            command[command_len - 1] = '\0';
-        }
+        // // 去除命令末尾的换行符(纯粹的废物行)
+        // size_t command_len = strlen(command);
+        // if (command[command_len - 1] == '\n') {
+        //     command[command_len - 1] = '\0';
+        // }
         
         // Check the user input command
         if (strcmp(command, "cd\n") == 0) {
@@ -204,14 +227,8 @@ int main() {
         // } else if (sscanf(command, "whereis %s", wheisCommand) == 1) {
         //     searchFiles(wheisCommand);
         } else {
-            // If the user input is not cd, cd xxx, ls, help, open, or rm command, then proceed with the original flow
             sscanf(command, "%d", &choice);
             switch (choice) {
-                case 2:
-                    printf("Enter filename to create: ");
-                    scanf("%s", filename);
-                    createFile(filename);
-                    break;
                 case 0:
                     printf("Exiting...\n");
                     exit(0);
--- src/Python/help.py
+++ src/Python/help.py
@@ -9,15 +9,15 @@ def help_log():
             rm -f <file_name> - Delete a file
             open <file_name> - Open a file
             cd <directory> - Change the current directory
-            (unaccessable)whereis <file_name> - Search for a file
-            (unaccessable)everything <file_name>- Search for files using Everything
+            whereis <file_name> - Search for a file
+            everything <file_name>- Search for files using Everything
             neofetch - Display system information
-            (unaccessable)fetchsoft - List all installed software
-            (unaccessable)unsoft <software_name> - Uninstall a software
-            (unaccessable)clear - Clear the screen
-            (unaccessable)diskpart - Open diskpart
-            (unaccessable)chkssd - Check SSD health
-            (unaccessable)quit - Exit the shell
-            (unaccessable)exit - Exit the shell
+            fetchsoft - List all installed software
+            unsoft <software_name> - Uninstall a software
+            clear - Clear the screen
+            diskpart - Open diskpart
+            chkssd - Check SSD health
+            quit - Exit the shell
+            exit - Exit the shell
             help - Display this help message
             """)
