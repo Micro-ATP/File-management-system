829026840296b85175cd0b78f98666e92812a3a4
陆岩淅 <131521928+Micro-ATP@users.noreply.github.com>
2024-02-23 09:47:17 +0800

    修复单独回车导致的程序运行故障，调整结构

--- src/C/main.c
+++ src/C/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-// #include <E:\GitHub\File-management-system\src\C\help.h>
+#include <E:\GitHub\File-management-system\src\C\help.h>
 #include <E:\GitHub\File-management-system\src\C\neofetch.h>
 
 void createFile(char *filename);
@@ -149,28 +149,6 @@ void changeDirectory(char *newDirectory) {
 //     FindClose(hFind);
 // }
 
-void help_log() {
-    printf("\n");
-    printf("Welcome to ATP_Shell, the friendly interactive shell !!!\n\n");
-    printf("    Commands example:\n\n");
-    printf("        ls - List files and directories in the current directory\n");
-    printf("        touch <file_name> - Create a new file\n");
-    printf("        rm -f <file_name> - Delete a file\n");
-    printf("        open <file_name> - Open a file\n");
-    printf("        cd <directory> - Change the current directory\n");
-    printf("        whereis <file_name> - Search for a file\n");
-    printf("        everything <file_name> - Search for files using Everything\n");
-    printf("        neofetch - Display system information\n");
-    printf("        fetchsoft - List all installed software\n");
-    printf("        unsoft <software_name> - Uninstall a software\n");
-    printf("        clear - Clear the screen\n");
-    printf("        diskpart - Open diskpart\n");
-    printf("        chkssd - Check SSD health\n");
-    printf("        quit - Exit the shell\n");
-    printf("        exit - Exit the shell\n");
-    printf("        help - Display this help message\n");
-    printf("\n");
-}
 
 int main() {
     int choice;
@@ -196,12 +174,11 @@ int main() {
         // 读取命令
         fgets(command, sizeof(command), stdin);
 
-        // // 去除命令末尾的换行符(纯粹的废物行)
-        // size_t command_len = strlen(command);
-        // if (command[command_len - 1] == '\n') {
-        //     command[command_len - 1] = '\0';
-        // }
-        
+        if (strcmp(command, "\n") == 0) {
+        continue; // 如果用户只输入了回车，则继续循环等待用户输入
+}
+
+
         // Check the user input command
         if (strcmp(command, "cd\n") == 0) {
             // If the user input is just "cd" command, then change directory to "C:\"
